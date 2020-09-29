5f046111650f156b8baac2f8de90eb5056e286cd
Rohit Mathew <rohit.mathew@turtlemint.com>
2020-09-29 22:35:02 +0530

    Initial Commit
    Initial commit with code setup

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const getComment = require('./lib/getComment')
+const defaultConfig = require('./lib/defaultConfig')
+const PullRequestBodyChecker = require('./lib/PullRequestBodyChecker')
+const IssueBodyChecker = require('./lib/IssueBodyChecker')
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
@@ -6,10 +11,68 @@ module.exports = app => {
   // Your code here
   app.log.info('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+  app.on(['pull_request.opened'], receive)
+  async function receive (context) {
+    let title
+    let body
+    let badTitle
+    let badBody
+    let user
+
+    let eventSrc = 'issue'
+    if (context.payload.pull_request) {
+      ({title, body, user} = context.payload.pull_request)
+      eventSrc = 'pullRequest'
+    } else {
+      ({title, body, user} = context.payload.issue)
+    }
+
+    try {
+      const config = await context.config(`config.yml`, defaultConfig)
+
+      if (!config.probotTemplateEnforcerOn[eventSrc]) {
+        return
+      }
+
+      if (config.probotTemplateEnforcerDefaultTitles) {
+        if (config.probotTemplateEnforcerDefaultTitles.includes(title.toLowerCase())) {
+          badTitle = true
+        }
+      }
+
+      if (eventSrc === 'pullRequest') {
+        app.log.info('Pull Request')
+        if (config.checkPullRequestTemplate && !(await PullRequestBodyChecker.isBodyValid(body, context))) {
+          badBody = true
+        }
+      } else if (eventSrc === 'issue') {
+        if (config.checkIssueTemplate && !(await IssueBodyChecker.isBodyValid(body, context))) {
+          badBody = true
+        }
+      }
+
+      let notExcludedUser = true
+      if (config.probotTemplateEnforcerUserstoExclude) {
+        if (config.probotTemplateEnforcerUserstoExclude.includes(user.login)) {
+          notExcludedUser = false
+        }
+      }
+
+      if ((!body || badTitle || badBody) && notExcludedUser) {
+        const comment = getComment(config.probotTemplateEnforcerReplyComment, defaultConfig.probotTemplateEnforcerReplyComment)
+        context.github.issues.createComment(context.issue({body: comment}))
+
+        if (config.probotTemplateEnforcerLabelToAdd) {
+          // Add label if there is one listed in the yaml file
+          context.github.issues.addLabels(context.issue({labels: [config.probotTemplateEnforcerLabelToAdd]}))
+        }
+      }
+    } catch (err) {
+      if (err.code !== 404) {
+        throw err
+      }
+    }
+  }
 
   // For more information on building apps:
   // https://probot.github.io/docs/
